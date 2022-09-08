6aff94bd735a1c09b494bbacd143544a937fef16
mirailisc <mirailisclm@gmail.com>
2022-09-08 14:15:48 +0700

    hotfix css

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -3,10 +3,8 @@ import styles from './index.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faFacebook, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { IconButton, Link, useColorMode } from '@chakra-ui/react'
+import { IconButton, Link } from '@chakra-ui/react'
 const Contact = () => {
-  const { colorMode } = useColorMode()
-
   return (
     <div className={styles.Contact}>
       <code>Contact Me</code>
@@ -14,7 +12,7 @@ const Contact = () => {
         <Link isExternal href="https://github.com/Mirailisc">
           <IconButton
             variant="ghost"
-            colorScheme={colorMode === 'light' ? 'orange' : 'blue'}
+            colorScheme={'blue'}
             aria-label="Github"
             icon={<FontAwesomeIcon icon={faGithub} />}
           />
@@ -22,7 +20,7 @@ const Contact = () => {
         <Link isExternal href="https://www.facebook.com/MirailiscLm/">
           <IconButton
             variant="ghost"
-            colorScheme={colorMode === 'light' ? 'orange' : 'blue'}
+            colorScheme={'blue'}
             aria-label="Facebook"
             icon={<FontAwesomeIcon icon={faFacebook} />}
           />
@@ -30,7 +28,7 @@ const Contact = () => {
         <Link isExternal href="https://twitter.com/Mirailisc">
           <IconButton
             variant="ghost"
-            colorScheme={colorMode === 'light' ? 'orange' : 'blue'}
+            colorScheme={'blue'}
             aria-label="Twitter"
             icon={<FontAwesomeIcon icon={faTwitter} />}
           />
@@ -38,7 +36,7 @@ const Contact = () => {
         <Link isExternal href="https://www.linkedin.com/in/phubordin-poolnai-31480a214/">
           <IconButton
             variant="ghost"
-            colorScheme={colorMode === 'light' ? 'orange' : 'blue'}
+            colorScheme={'blue'}
             aria-label="LinkedIn"
             icon={<FontAwesomeIcon icon={faLinkedin} />}
           />
@@ -46,7 +44,7 @@ const Contact = () => {
         <Link isExternal href="mailto: mirailisclm@gmail.com">
           <IconButton
             variant="ghost"
-            colorScheme={colorMode === 'light' ? 'orange' : 'blue'}
+            colorScheme={'blue'}
             aria-label="LinkedIn"
             icon={<FontAwesomeIcon icon={faEnvelope} />}
           />
